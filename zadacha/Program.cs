fd47f29ed328820636b9cbfae26b692471e2dbe6
Dmitriy <zeifd@mail.ru>
2022-06-19 20:10:47 +0700

    Создание функции для формирования массива со значениями строк меньше либо равным 3

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -8,24 +8,28 @@ void Print(string[] array)                                 // вывод на э
         Console.Write(array[i] + " ");
     }
 }
+string[] Mass3(string[] array)
+{
+    int kolstr = 0;                                          // задаем переменную для хранения количества строк с количеством символов меньше либо равным 3
+    for (int i = 0; i < array.Length; i++)                   // узнаем количество строк с количеством символов меньше либо равным 3
+    {
+        if (array[i].Length <= 3)
+        {
+            kolstr++;
+        }
+    }
+    string[] array3 = new string[kolstr];                    // создание массива с найденым количеством строк
+    int str = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array3[str] = array[i];
+            str++;
+        }
+    }
+    return array3;
+}
 
 string[] array = { "hello", "2", "world", ":-)" };       // задаем массив с произольными значениями
-int kolstr = 0;                                          // задаем переменную для хранения количества строк с количеством символов меньше либо равным 3
-for (int i = 0; i < array.Length; i++)                   // узнаем количество строк с количеством символов меньше либо равным 3
-{
-    if (array[i].Length <= 3)
-    {
-        kolstr++;
-    }
-}
-string[] array3 = new string[kolstr];                    // создание массива с найденым количеством строк
-int str = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length <= 3)
-    {
-        array3[str] = array[i];
-        str++;
-    }
-}
-Print(array3);
+Print(Mass3(array));
